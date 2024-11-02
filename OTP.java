23e047e9bab587e00b7299cbe0daf60e9e380f8e
Immanuel.D <immanuelrobinson08@gmail.com>
2024-11-02 14:07:50 +0530

    OTP.java
    A basic OTP generator!

---
+++ OTP.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class OTP{
+    public static void main(String[] args){
+        // 6-digit OTP generator for 10-digit mobile number!
+
+        Scanner input = new Scanner(System.in);
+        Random random = new Random();
+
+        System.out.print("Enter the mobile number: ");
+        int num = input.nextInt();
+
+        String ph_num = Integer.toString(num);
+
+        int rand_int = random.nextInt(100000, 1000000);
+
+        if (ph_num.length() == 10){
+            System.out.printf("The OTP for the mobile number %s is %d", ph_num, rand_int);
+        }
+        else {
+            System.out.println("Enter a valid mobile number");
+        }
+        
+
+    }
+}
