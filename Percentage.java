4ecaff5eb4c0711420f1dd940dd7567f4c5c98fd
Immanuel.D <immanuelrobinson08@gmail.com>
2024-11-17 13:49:34 +0530

    Percentage.java
    Updated with the double return method!

--- Percentage.java
+++ Percentage.java
@@ -2,15 +2,17 @@ public class Percentage {
 	
 	public static void main(String[] args) {
 		
-		percentage(/*The percentage*/, /*The number*/);
+		percentage(43, 234);
 		
 	}
 	
-	static void percentage(double n1, double n2) {
+	static double percentage(double n1, double n2) {
 		
 		double p = (n1 / 100) * n2;
+		
 		System.out.printf("%.1f%% of %.1f is %.1f", n1, n2, p);
 		
+		return 0;
 	}
 	
 }
