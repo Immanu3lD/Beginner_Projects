fc3015b911b97d2c3ee22ef6bdad4d0710b6181e
Immanuel.D <immanuelrobinson08@gmail.com>
2024-11-17 13:43:33 +0530

    Calculator.java
    Updated with exception handling!

--- Calculator.java
+++ Calculator.java
@@ -15,36 +15,96 @@ public class Calculator{
             String operator = input.next().toLowerCase();
 
             if (operator.equals("a")){
-                System.out.print("Enter the 1st number to add: ");
-                int num1 = input.nextInt();
-                System.out.print("Enter the 2nd number to add: ");
-                int num2 = input.nextInt();
-                int num = num1 + num2;
-                System.out.printf("%d + %d = %d\n", num1, num2, num);
+            	try {
+            		System.out.print("Enter the 1st number to add: ");
+                    double num1 = input.nextDouble();
+                    System.out.print("Enter the 2nd number to add: ");
+                    double num2 = input.nextDouble();
+                    double num = num1 + num2;
+                    System.out.printf("%.2f + %.2f = %.2f\n", num1, num2, num);
+            	}
+            	catch (InputMismatchException e) {
+            		System.out.println("Enter only numbers plz!");
+            		System.out.println(e);
+            		
+                    String dec = input.next().toLowerCase();
+
+                    if (dec.equals("y")){
+                        program = true;
+                    }
+                    else {
+                        program = false;
+                    }
+            	}
             }
             else if (operator.equals("s")){
-                System.out.print("Enter the number to subtract: ");
-                int num1 = input.nextInt();
-                System.out.print("Enter the number to subtracted by: ");
-                int num2 = input.nextInt();
-                int num = num1 - num2;
-                System.out.printf("%d - %d = %d\n", num1, num2, num);
+            	try {
+            		System.out.print("Enter the number to subtract: ");
+            		double num1 = input.nextDouble();
+            		System.out.print("Enter the number to subtracted by: ");
+            		double num2 = input.nextDouble();
+            		double num = num1 - num2;
+            		System.out.printf("%.2f - %.2f = %.2f\n", num1, num2, num);
+            	}
+            	catch (InputMismatchException e) {
+            		System.out.println("Enter only numbers plz!");
+            		System.out.println(e);
+            		
+            		String dec = input.next().toLowerCase();
+
+                    if (dec.equals("y")){
+                        program = true;
+                    }
+                    else {
+                        program = false;
+                    }	
+            	}
             }
             else if (operator.equals("m")){
-                System.out.print("Enter the number to multiply: ");
-                int num1 = input.nextInt();
-                System.out.print("Enter the number to multiplied by: ");
-                int num2 = input.nextInt();
-                int num = num1 * num2;
-                System.out.printf("%d x %d = %d\n", num1, num2, num);
+            	try {
+            		System.out.print("Enter the number to multiply: ");
+            		double num1 = input.nextDouble();
+            		System.out.print("Enter the number to multiplied by: ");
+            		double num2 = input.nextDouble();
+            		double num = num1 * num2;
+            		System.out.printf("%.2f x %.2f = %.2f\n", num1, num2, num);
+            	}
+            	catch (InputMismatchException e) {
+            		System.out.println("Enter only numbers plz!");
+            		System.out.println(e);
+            		
+            		String dec = input.next().toLowerCase();
+
+                    if (dec.equals("y")){
+                        program = true;
+                    }
+                    else {
+                        program = false;
+                    }   
+            	}
             }
             else if (operator.equals("d")) {
-                System.out.print("Enter the number to divide: ");
-                double num1 = input.nextDouble();
-                System.out.print("Enter the number to divided by: ");
-                double num2 = input.nextDouble();
-                double num = num1 / num2;
-                System.out.printf("%.2f / %.2f = %.2f\n", num1, num2, num);
+            	try {
+            		System.out.print("Enter the number to divide: ");
+                    double num1 = input.nextDouble();
+                    System.out.print("Enter the number to divided by: ");
+                    double num2 = input.nextDouble();
+                    double num = num1 / num2;
+                    System.out.printf("%.2f / %.2f = %.2f\n", num1, num2, num);
+            	}
+            	catch (InputMismatchException e) {
+            		System.out.println("Enter only numbers plz!");
+            		System.out.println(e);
+            		
+            		String dec = input.next().toLowerCase();
+
+                    if (dec.equals("y")){
+                        program = true;
+                    }
+                    else {
+                        program = false;
+                    }
+            	}
             }
             else {
                 System.out.println("Enter the valid input!");
